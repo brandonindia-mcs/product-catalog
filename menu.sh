0ec6f0c70ceb5e78860d1b0f69e4a0b1e4dd80c4
Brandon R. India <brandonindia-mcs>
2025-10-08 20:37:34 -0500

    fixed gaps in menu

--- menu.sh
+++ menu.sh
@@ -21,12 +21,11 @@
   function run_system_check { system_check && echo -e "\n\tsdenv is $sdenv\n\tDOCKERHUB is $DOCKERHUB" ; }
   function system_check { . ./bootstrap.sh && setenv; }
 
-
   function run_configure_webservice() { GLOBAL_NAMESPACE=$1 configure_webservice $2 ; }
   function run_configure_api() { GLOBAL_NAMESPACE=$1 configure_api $2 ; }
   function run_configure_postgre() { GLOBAL_NAMESPACE=$1 configure_postgre $2 ; }
 
-  function run_frontend() { frontend ;}
+  function run_frontend_18() { frontend_18 ;}
   function run_middleware() { middleware ;}
   function run_backend() { echo && echo menu.js: not overwriting postgres && return 1; backend; }
 
@@ -43,23 +42,23 @@
   function run_update_webservice() { GLOBAL_NAMESPACE=$1 update_webservice $2 ; }
   function run_install_api() { GLOBAL_NAMESPACE=$1 install_api $2; }
   function run_install_postgres() { GLOBAL_NAMESPACE=$1 install_postgres $2; }
-  function run_validate_api() { validate_api; }
+  # function run_validate_api() { validate_api; }
 
   function show_menu() {
     namespace=default && image_version="$namespace-$(version)"
     echo tag is: $image_version
     echo -e "\nSelect an option:"
     echo -e "10) sys_check \t*) Exit"
-    echo -e "20) install_webservice  \t21) frontend   \t22) update_webservice \t23) image_frontend   \t24) configure_webservice  \t25) k8s_webservice"
-    echo -e "30) install_api         \t31) middleware \t32) validate_api       \t33) image_middleware \t34) configure_api         \t35) k8s_api"
+    echo -e "20) install_webservice \t21) frontend_18\t22) update_webservice\t23) image_frontend  \t24) configure_webservice \t25) k8s_webservice"
+    echo -e "30) install_api        \t31) middleware \t32) validate_api     \t33) image_middleware\t34) configure_api        \t35) k8s_api"
     # echo -e "40) backend \t 41) install_postgres"
-    echo -e "40) install_postgres    \t               \t                       \t43) image_backend    \t44) configure_postgre     \t45) k8s_postgre"
+    echo -e "40) install_postgres   \t               \t                     \t43) image_backend    \t44) configure_postgre   \t45) k8s_postgre"
     # echo -e "90) net new install"
     echo && read -p "Enter choice or exit: " choice
 
     case $choice in
       10) run_system_check ;;
-      21) system_check && run_frontend ;;
+      21) system_check && run_frontend_18 ;;
       20) system_check && run_install_webservice $namespace $image_version ;;
       22) system_check && run_update_webservice $namespace $image_version ;;
       24) system_check && run_configure_webservice $namespace $image_version ;;
@@ -75,7 +74,7 @@
       40) system_check && run_install_postgres $namespace $image_version ;;
       44) system_check && run_configure_postgre $namespace $image_version ;;
       45) system_check && run_k8s_postgres $namespace ;;
-      45) system_check && run_build_image_backend $image_version ;;
+      43) system_check && run_build_image_backend $image_version ;;
       90) system_check && run_install_all $namespace $image_version ;;
       *) echo "Exiting..."; exit 0 ;;
     esac
