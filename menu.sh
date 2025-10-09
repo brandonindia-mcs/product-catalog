ae3068e26c306a9495bb31f7bfe77247645515f5
Brandon R. India <brandonindia-mcs>
2025-10-08 20:14:52 -0500

    enhanced menu

--- menu.sh
+++ menu.sh
@@ -20,43 +20,62 @@
   }
   function run_system_check { system_check && echo -e "\n\tsdenv is $sdenv\n\tDOCKERHUB is $DOCKERHUB" ; }
   function system_check { . ./bootstrap.sh && setenv; }
-  function run_upgrade_webservice() { GLOBAL_NAMESPACE=$1 upgrade_webservice $2 ; }
-  function run_install_webservice() { GLOBAL_NAMESPACE=$1 install_webservice $2 ; }
+
+
   function run_configure_webservice() { GLOBAL_NAMESPACE=$1 configure_webservice $2 ; }
+  function run_configure_api() { GLOBAL_NAMESPACE=$1 configure_api $2 ; }
+  function run_configure_postgre() { GLOBAL_NAMESPACE=$1 configure_postgre $2 ; }
+
   function run_frontend() { frontend ;}
   function run_middleware() { middleware ;}
-  function run_install_api() { GLOBAL_NAMESPACE=$1 install_api $2; }
-  function run_configure_api() { GLOBAL_NAMESPACE=$1 configure_api $2 ; }
-  function run_validate_api() { validate_api; }
   function run_backend() { echo && echo menu.js: not overwriting postgres && return 1; backend; }
-  function run_install_postgres() { echo && echo menu.js: not overwriting postgres && return 1; GLOBAL_NAMESPACE=$1 install_postgres $2; }
-  function run_configure_postgre() { GLOBAL_NAMESPACE=$1 configure_postgre $2 ; }
+
+  function run_build_image_frontend() { build_image_frontend $1; }
+  function run_build_image_middleware() { build_image_middleware $1; }
+  function run_build_image_backend() { build_image_backend $1; }
+
+  function run_k8s_webservice() { GLOBAL_NAMESPACE=$1 k8s_webservice ; }
+  function run_k8s_api() { GLOBAL_NAMESPACE=$1 k8s_api ; }
+  function run_k8s_postgres() { GLOBAL_NAMESPACE=$1 k8s_postgres ; }
+
   
+  function run_install_webservice() { GLOBAL_NAMESPACE=$1 install_webservice $2 ; }
+  function run_upgrade_webservice() { GLOBAL_NAMESPACE=$1 upgrade_webservice $2 ; }
+  function run_install_api() { GLOBAL_NAMESPACE=$1 install_api $2; }
+  function run_install_postgres() { GLOBAL_NAMESPACE=$1 install_postgres $2; }
+  function run_validate_api() { validate_api; }
+
   function show_menu() {
     namespace=default && image_version="$namespace-$(version)"
     echo tag is: $image_version
     echo -e "\nSelect an option:"
     echo -e "10) sys_check \t*) Exit"
-    echo -e "20) frontend   \t21) install_webservice \t22) upgrade_webservice \t23) configure_webservice"
-    echo -e "30) middleware \t31) install_api        \t32) validate_api       \t33) configure_api"
+    echo -e "20) install_webservice  \t21) frontend   \t22) upgrade_webservice \t23) image_frontend   \t24) configure_webservice  \t25) k8s_webservice"
+    echo -e "30) install_api         \t31) middleware \t32) validate_api       \t33) image_middleware \t34) configure_api         \t35) k8s_api"
     # echo -e "40) backend \t 41) install_postgres"
-    echo -e "               \t                       \t                       \t43) configure_postgre"
+    echo -e "40) install_postgres    \t               \t                       \t43) image_backend    \t44) configure_postgre     \t45) k8s_postgre"
     # echo -e "90) net new install"
-    read -p "Enter choice or exit: " choice
+    echo && read -p "Enter choice or exit: " choice
 
     case $choice in
       10) run_system_check ;;
-      20) system_check && run_frontend ;;
-      21) system_check && run_install_webservice $namespace $image_version ;;
+      21) system_check && run_frontend ;;
+      20) system_check && run_install_webservice $namespace $image_version ;;
       22) system_check && run_upgrade_webservice $namespace $image_version ;;
-      23) system_check && run_configure_webservice $namespace $image_version ;;
-      30) system_check && run_middleware ;;
-      31) system_check && run_install_api $namespace $image_version ;;
+      24) system_check && run_configure_webservice $namespace $image_version ;;
+      25) system_check && run_k8s_webservice $namespace ;;
+      23) system_check && run_build_image_frontend $image_version ;;
+      31) system_check && run_middleware ;;
+      30) system_check && run_install_api $namespace $image_version ;;
       32) system_check && run_validate_api ;;
-      33) system_check && run_configure_api $namespace $image_version ;;
-      40) system_check && run_backend $namespace $image_version ;;
-      41) system_check && run_install_postgres $namespace $image_version ;;
-      43) system_check && run_configure_postgre $namespace $image_version ;;
+      34) system_check && run_configure_api $namespace $image_version ;;
+      35) system_check && run_k8s_api $namespace ;;
+      33) system_check && run_build_image_middleware $image_version ;;
+      41) system_check && run_backend $namespace $image_version ;;
+      40) system_check && run_install_postgres $namespace $image_version ;;
+      44) system_check && run_configure_postgre $namespace $image_version ;;
+      45) system_check && run_k8s_postgres $namespace ;;
+      45) system_check && run_build_image_backend $image_version ;;
       90) system_check && run_install_all $namespace $image_version ;;
       *) echo "Exiting..."; exit 0 ;;
     esac
